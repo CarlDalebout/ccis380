9cc9c89ff42cc257e54e560b609758afe1e4607b
Carl Dalebout <cadalebout1@couger.ciss.edu>
2023-09-01 12:00:55 -0500

    Finished a01q04

--- a/a01/a01q04/complex.cpp
+++ a/a01/a01q04/complex.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "complex.h"
 
 double complex::re() const
@@ -11,67 +12,94 @@ double complex::im() const
 
 complex & complex::operator+=(const complex & z)
 {
-    return z;
-}
+    complex temp0 = *this;
+    complex temp1 = z;
+    *this = temp0 + temp1;
+    return *this;
+}   
 complex & complex::operator-=(const complex & z)
 {
-    return z;
+    complex temp0 = *this;
+    complex temp1 = z;
+    *this = temp0 - temp1;
+    return *this;
 }
 complex & complex::operator*=(const complex & z)
-{
-    return z;
+{  
+    complex temp0 = *this;
+    complex temp1 = z;
+    *this = temp0 * temp1;
+    return *this;
 }
 complex & complex::operator/=(const complex & z)
 {
-    return z;
+    complex temp0 = *this;
+    complex temp1 = z;
+    *this = temp0 / temp1;
+    return *this;
 }
+
+
+
 complex complex::operator+(const complex & z) const
 {
-    return z;
+    return complex((re_ + z.re()), (im_ + z.im()));
 }
 complex complex::operator-(const complex & z)
 {
-    return z;
+    return complex((re_ - z.re()), (im_ - z.im()));
 }
 complex complex::operator*(const complex & z)
 {
-    return z;
+    return complex( ( (re_ * z.re()) - (im_ * z.im()) ), ( (re_ * z.im()) + (im_ * z.re()) ));
 }
 complex complex::operator/(const complex & z) const
-{
-    return z;
-}
+{   
+    complex temp1( (re_ / (pow(z.re(), 2) + pow(z.im(), 2))), (im_ / (pow(z.re(), 2) + pow( z.im(), 2))) );
+    complex temp2(z.re(), -z.im());
+    return complex(temp1 * temp2);
+}   
 double complex::abs() const
 {
-    return 1.0;
+    return sqrt(pow(re_, 2) + pow(im_, 2));
 }
 
 
 complex operator+(double x, const complex & z)
 {
-    return z;
+    return complex((x + z.re()), (z.im()));
 }
 complex operator-(double x, const complex & z)
 {
-    return z;
+    return complex((x - z.re()), (z.im()));
 }
 complex operator*(double x, const complex & z)
 {
-    return z;
+    return complex( (x * z.re()), (x * z.im()) ) ;
 }
 complex operator/(double x, const complex & z)
 {
-    return z;
+    complex temp1( ( x / (pow(z.re(), 2) + pow(z.im(), 2))) , 0 );
+    complex temp2( z.re(), -z.im());
+    return temp1 * temp2;
 }
 double abs(const complex z)
 {
-    return 1.0;
+    return sqrt(pow(z.re(), 2) + pow(z.im(), 2));
 }
-std::istream & operator>>(std::istream & cout, complex & z)
+std::istream & operator>>(std::istream & cin, complex & z)
 {
-    return cout;
+    double re, im;
+    cin >> re >> im;
+    complex temp(re, im);
+    z = temp;
+    return cin;
 }
 std::ostream & operator<<(std::ostream & cout, const complex & z)
 {
+    if(z.im() >= 0)
+        cout << z.re()  << " + " << z.im() << 'i';
+    else
+        cout << z.re() << " - " << -z.im() << 'i';
     return cout;
 }
--- a/a01/a01q04/main.cpp
+++ a/a01/a01q04/main.cpp
@@ -1,8 +1,95 @@
 #include <iostream>
+#include <string>
 #include "complex.h"
+
+std::string removeSpaces(std::string input)
+{
+    int c = 0;
+    for(int i = 0; i <= input.length(); ++i)
+    {
+        if(input[i] != ' ')
+        {
+            //std::cout << "found " << input[i] << "c++\n";
+            c++;
+        }
+        else
+        {
+            int j = i+1;
+            while(input[j] == ' ')
+            {
+                if(j >= input.length())
+                {
+                    //std::cout << "Resizing input from " << input.length() << " to " << input.length() - c << std::endl;
+                    input.resize(c);
+                    return input;            
+                }
+                ++j;
+            }
+            //std::cout << "found " << input[j] << "c++\n";
+            c++;
+            char temp = input[i];
+            input[i] = input[j];
+            input[j] = temp;
+        }
+    }
+    //std::cout << "Resizing input from " << input.length() << " to " << input.length() - c << std::endl;
+    input.resize(c);
+    return input;
+}
 
 int main()
 {
+    std::string input;
+    std::getline(std::cin, input);
+    input = removeSpaces(input);
+
+    std::cout << '"' << input << '"' << std::endl;
+    std::cout << "recived " << int(input[0])-48 << std::endl;
+
+    complex z0(int(input[1])-48, int(input[2])-48);
+    complex x1(int(input[3])-48, int(input[4])-48);
+    
+    std::cout << "z0: " << z0 << "\nz1: " << x1 << std::endl; 
+
+    switch(int(input[0])-48)
+    {
+        case 0:
+            std::cout << "case 0\n";
+            std::cout << (z0 += x1) << std::endl;
+            break;
+        case 1:
+            std::cout << "case 1\n";
+            std::cout << (z0 -= x1) << std::endl;
+            break;
+        case 2:
+            std::cout << "case 2\n";
+            std::cout << (z0 *= x1) << std::endl;
+            break;
+        case 3:
+            std::cout << "case 3\n";
+            std::cout << (z0 /= x1) << std::endl;
+            break;
+        case 4:
+            std::cout << "case 4\n";
+            std::cout << (z0 + x1) << std::endl;
+            break;
+        case 5:
+            std::cout << "case 5\n";
+            std::cout << (z0 - x1) << std::endl;
+            break;
+        case 6:
+            std::cout << "case 6\n";
+            std::cout << (z0 * x1) << std::endl;
+            break;
+        case 7:
+            std::cout << "case 7\n";
+            std::cout << (z0 / x1) << std::endl;
+            break;
+        case 8:
+            std::cout << "case 8\n";
+            std::cout << (z0.abs()) <<std::endl;
+            break;
+    }
 
     return 0;
 }

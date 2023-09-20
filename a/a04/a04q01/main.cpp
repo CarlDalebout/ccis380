c2fb668843965204571af19380a0f30c2f85994c
CarlDalebout <cadalebout1@couger.ciss.edu>
2023-09-20 12:04:35 -0500

    added keyboard function to a04q01

--- a/a04/a04q01/main.cpp
+++ a/a04/a04q01/main.cpp
@@ -4,6 +4,13 @@
 #include <GL/freeglut.h>
 #include "../mygllib/gl2d.h"
 
+
+float cx = 200.0f;
+float cy = 198.0f;
+float rx = 50.0f;
+float ry = 50.0f;
+float dt = 0.1f;
+
 struct point
 {
     float x;
@@ -32,25 +39,18 @@ void circle(float x, float y, float r, int num_points=-1)
         regular_polygon(x, y, r, num_points);
 }
 
-void arc(float x, float y, float r, int num_points,
+void arc(float x, float y, float r, int num_points, float line_width,
          float start_angle = 0.0f,
          float end_angle = 2 * 3.14159)
 {
     glBegin(GL_LINE_STRIP);
+    glLineWidth(line_width);
     glVertex2f(r*cos(start_angle)+x, r*sin(start_angle)+y);
-
-    glColor3f(0, 1, 0);
-    if(start_angle <= end_angle)
-        for(float i = start_angle; i <= end_angle; i += end_angle/num_points)
-        {
-            glVertex2f(r*cos(i)+x, r*sin(i)+y);
-        }
-    else
-        for(float i = end_angle; i <= start_angle; i += start_angle/num_points)
-        {
-            glVertex2f(r*cos(i)+x, r*sin(i)+y); 
-        }
-    
+ 
+    for(float i = start_angle; i <= end_angle; i += end_angle/num_points)
+    {
+        glVertex2f(r*cos(i)+x, r*sin(i)+y);
+    }    
     glEnd();
 }
 
@@ -58,21 +58,71 @@ void filled_arc(float x, float y, float r, int num_points,
                    float start_angle = 0.0f,
                    float end_angle = 2 * 3.14159)
 {
-    glBegin(GL_QUADS);
-    glVertex2f(r*cos(start_angle)+x, r*sin(start_angle)+y);
+    glBegin(GL_POLYGON);
 
-    glColor3f(0, 1, 0);
-    if(start_angle <= end_angle)
-        for(float i = start_angle; i <= end_angle; i += end_angle/num_points)
-        {
-            glVertex2f(r*cos(i)+x, r*sin(i)+y);
-        }
-    else
-        for(float i = end_angle; i <= start_angle; i += start_angle/num_points)
-        {
-            glVertex2f(r*cos(i)+x, r*sin(i)+y); 
-        }
-    
+    glVertex2f(r*cos(start_angle)+x, r*sin(start_angle)+y);
+    for(float i = start_angle; i <= end_angle+(end_angle/num_points); i += end_angle/num_points)
+    {
+        glVertex2f(r*cos(i)+x, r*sin(i)+y);
+    }    
+    glEnd();
+}
+
+void Ellipse(float cx, float cy, float rx, float ry, int num_segments) 
+{ 
+    float theta = 2 * 3.1415926 / float(num_segments); 
+    float c = cosf(theta);//precalculate the sine and cosine
+    float s = sinf(theta);
+    float t;
+
+    float x = 1;//we start at angle = 0 
+    float y = 0; 
+
+    glBegin(GL_LINE_LOOP); 
+    for(int i = 0; i < num_segments; i++) 
+    { 
+        //apply radius and offset
+        glVertex2f(x * rx + cx, y * ry + cy);//output vertex 
+
+        //apply the rotation matrix
+        t = x;
+        x = c * x - s * y;
+        y = s * t + c * y;
+    } 
+    glEnd(); 
+}
+
+void Filled_Ellipse(float cx, float cy, float rx, float ry, int num_segments) 
+{ 
+    float theta = 2 * 3.1415926 / float(num_segments); 
+    float c = cosf(theta);//precalculate the sine and cosine
+    float s = sinf(theta);
+    float t;
+
+    float x = 1;//we start at angle = 0 
+    float y = 0; 
+
+    glBegin(GL_POLYGON); 
+    for(int i = 0; i < num_segments; i++) 
+    { 
+        //apply radius and offset
+        glVertex2f(x * rx + cx, y * ry + cy);//output vertex 
+
+        //apply the rotation matrix
+        t = x;
+        x = c * x - s * y;
+        y = s * t + c * y;
+    } 
+    glEnd(); 
+}
+
+void triangle(point p1, point p2, point p3)
+{
+    glBegin(GL_TRIANGLES);
+    glVertex2f(p1.x, p1.y);
+    glVertex2f(p2.x, p2.y);
+    glVertex2f(p3.x, p3.y);
+
     glEnd();
 }
 
@@ -115,16 +165,96 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     
+    //main body
+    glColor3f(0, 0, 0);
+    Filled_Ellipse(200, 200, 100, 100, 50);
     glColor3f(1, 0, 0);
-    filled_arc(200, 200, 100, 4, 0, 3.14159/4);
+    Filled_Ellipse(200, 200, 98, 98, 50);
+    
+    //belly
+    glColor3f(0, 0, 0); 
+    Filled_Ellipse(200, 126, 65, 24, 50);
+    glColor3f(0.94, 0.90, 0.55);
+    Filled_Ellipse(200, 126, 63, 22, 50);
+    
+    //eyes
+    glColor3f(0, 0, 0);
+    Filled_Ellipse(200, 210, 20, 20, 30);
+    Filled_Ellipse(240, 210, 20, 20, 30);
+    glColor3f(1, 1, 1);
+    Filled_Ellipse(200, 210, 18, 18, 30);
+    Filled_Ellipse(240, 210, 18, 18, 30);
+    glColor3f(0, 0, 0);
+    Filled_Ellipse(208, 210, 5, 5, 30);
+    Filled_Ellipse(232, 210, 5, 5, 30);
+    
+    //beak
+    glColor3f(0, 0, 0);
+    triangle({200, 180}, {220, 200}, {260, 170});
+    triangle({200, 180}, {220, 160}, {260, 170});
+    glColor3f(1, 0.55, 0);
+    triangle({203, 181}, {220, 197}, {254, 172.2});
+    
     glFlush();
 }
 
+void keyboard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 'w':
+        cy += dt;
+        std::cout << '(' << cx << ", " << cy << ')' << " rx: " << rx << " ry: " << ry << std::endl;
+        break;
+    case 's':
+        cy -= dt;
+        std::cout << '(' << cx << ", " << cy << ')' << " rx: " << rx << " ry: " << ry << std::endl; 
+        break;
+    case 'a':
+        cx -= dt;
+        std::cout << '(' << cx << ", " << cy << ')' << " rx: " << rx << " ry: " << ry << std::endl;
+        break;
+    case 'd':
+        cx += dt;
+        std::cout << '(' << cx << ", " << cy << ')' << " rx: " << rx << " ry: " << ry << std::endl;
+        break;
+    
+    case 'q':
+        rx -= dt;
+        std::cout << '(' << cx << ", " << cy << ')' << " rx: " << rx << " ry: " << ry << std::endl;
+        break;
+
+    case 'e':
+        rx += dt;
+        std::cout << '(' << cx << ", " << cy << ')' << " rx: " << rx << " ry: " << ry << std::endl;
+        break;
+
+    case 'o':
+        ry -= dt;
+        std::cout << '(' << x << ", " << y << ')' << " rx: " << rx << " ry: " << ry << std::endl;
+        break;
+    case 'p':
+        ry += dt;
+        std::cout << '(' << x << ", " << y << ')' << " rx: " << rx << " ry: " << ry << std::endl;
+        break;
+    case '+':
+        dt += 0.1;
+        std::cout << dt << std::endl;
+        break;
+    case '-':
+        if(dt != 0.1)
+            dt -= 0.1;
+        std::cout << dt << std::endl;
+        break;  
+    }
+    glutPostRedisplay();
+}
 
 int main()
 {
     mygllib::init2d(900, 900);
     glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
     glutMainLoop();
     return 0;
 }

05de605ff9d3a8e95c25f4de215e2f0c7b79afb7
Carl Dalebout <112584760+CarlDalebout@users.noreply.github.com>
2023-11-10 23:10:37 -0600

    Update Heightmap.h

--- a/a09/Heightmap.h
+++ a/a09/Heightmap.h
@@ -14,21 +14,21 @@ public:
     Heightmap(int n)
     :n_(n)
     {
-        std::vector<std::vector<float>> temp(pow(2, n)+1, std::vector<float>(pow(2, n)+1, 0));
+        std::vector<std::vector<double>> temp(pow(2, n)+1, std::vector<double>(pow(2, n)+1, 0));
         heightmap_ = temp;
     }
 
     int & n()       {return n_;}
     int   n() const {return n_;}
-    std::vector<std::vector<float>> & heightmap()       {return heightmap_;}
-    std::vector<std::vector<float>>   heightmap() const {return heightmap_;}
+    std::vector<std::vector<double>> & heightmap()       {return heightmap_;}
+    std::vector<std::vector<double>>   heightmap() const {return heightmap_;}
 
     void                              Diamond_Square();
     Heightmap                         resize(int n);
 
 private:
     int     n_;
-    std::vector <std::vector<float>> heightmap_;
+    std::vector <std::vector<double>> heightmap_;
 };
     std::ostream & operator<<(std::ostream& cout, const Heightmap& heightmap);
 
